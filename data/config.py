e7b669295d9c45367102380fb8907df2705b1403
Shokhrukh <103505718+Shokhrukh2004@users.noreply.github.com>
2022-04-14 17:38:12 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,10 +1,15 @@
-from environs import Env
+# from environs import Env
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
+# # Теперь используем вместо библиотеки python-dotenv библиотеку environs
+# env = Env()
+# env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
-CHANNELS = ["@multi_bot_uzb"]
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
+# ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+# IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+# CHANNELS = ["@multi_bot_uzb"]
+import os
+
+ADMINS = list(os.environ.get("ADMINS"))
+BOT_TOKEN=str(os.environ.get("BOT_TOKEN"))
+CHANNELS=str(os.environ.get("CHANNELS"))
